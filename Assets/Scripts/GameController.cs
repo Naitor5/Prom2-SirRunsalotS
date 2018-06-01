ad436c9304c2e96485293ca8adf3f94b8f9dde1a
Aleksei Palma <supersonic616@hotmail.com>
2018-06-01 14:00:52 -0500

    GC ya no hereda de Quest

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : Quest {
+public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
