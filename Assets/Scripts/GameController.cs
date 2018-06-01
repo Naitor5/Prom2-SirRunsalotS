6c1cf6f3e33da2ba3806bffa6a2d7874e6675e45
Aleksei Palma <supersonic616@hotmail.com>
2018-06-01 14:17:34 -0500

    GC actualizado

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UI;
 
 public class GameController : MonoBehaviour {
 
+	public InputFieldEditor IField;
+	 
+
 	// Use this for initialization
 	void Start () {
+
+		IField =  GetComponent<InputFieldEditor>(); 
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void CheckPregunta1()
+	{
+		 
 	}
 }
